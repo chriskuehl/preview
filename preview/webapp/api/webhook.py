ea40fcfbea6a3f79a01f47c6f21ebb9a0b3ac6a3
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-11-14 00:12:45 +0000

    Add some logging

--- preview/webapp/api/webhook.py
+++ preview/webapp/api/webhook.py
@@ -1,6 +1,7 @@
 """GitHub webhook handler."""
 import hashlib
 import hmac
+import logging
 
 from flask import Blueprint
 from flask import request
@@ -9,6 +10,7 @@ from preview.config import get_config
 from preview.github import create_status
 
 
+_logger = logging.getLogger(__name__)
 webhook = Blueprint('webhook', __name__)
 
 
@@ -35,15 +37,28 @@ def webhook_view():
       * Updated existing pull request:
         https://gist.github.com/anonymous/22f90074414c89d191ca
     """
-    if not _hmac_matches(request.data, request.headers.get('X-Hub-Signature')):
+
+    event_type = request.headers.get('X-Github-Event')
+    signature = request.headers.get('X-Hub-Signature')
+
+    _logger.debug('Received webhook with event_type={}'.format(event_type))
+
+    if not _hmac_matches(request.data, signature):
+        _logger.warning('Received bad webhook, HMAC validation failed for signature: {}'.format(signature))
         return '403 Forbidden', 403
 
-    if request.headers['X-Github-Event'] != 'pull_request':
+    if event_type != 'pull_request':
         return '200 OK', 200
 
     head_repo_name = request.json['pull_request']['head']['repo']['name']
     head_owner = request.json['pull_request']['head']['repo']['owner']['login']
     head_sha = request.json['pull_request']['head']['sha']
+
+    _logger.info('Handling pull request event; head_repo_name={}; head_owner={}; head_sha={}'.format(
+        head_repo_name,
+        head_owner,
+        head_sha,
+    ))
 
     create_status(
         head_owner,
